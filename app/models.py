61b7ce9d59896d45b48e85effdbb06b471002cf8
sergioraspo <sergioraspo@gmail.com>
2023-12-07 18:36:46 -0300

    makemigrations

--- app/models.py
+++ app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Person(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    phone= models.IntegerField()
+    
+class Evento(models.Model):
+    tipo= models.TextChoices("Casamiento", "Fiesta de 15 años")
+    fecha_evento= models.DateField(auto_now=False, auto_now_add=False)
+    horario=models.TextChoices("Diurno", "Nocturno")
+
+class Opciones(models.Model):
+    tipo= models.TextChoices("Foto mural", "Video con drone")
+
+def __str__(self):
+    return self.first_name
+    return self.last_name
+    return self.phone
+    
--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path, re_path, include
 from . import views
 
-urlpatterns = [
-    path('reserva/', views.index,name='inicio_app')
-   
+urlpatterns= [
+    path('Person/', views.index,name='inicio_app'),
+    path('Evento/', views.index,name='evento_app'),
+    path('Opciones/', views.index,name='opciones_app')
 ]
+
+
+
+
