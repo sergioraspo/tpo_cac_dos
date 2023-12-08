6b834f574e0156b47790141ddf342de9ff521361
sergioraspo <sergioraspo@gmail.com>
2023-12-08 16:51:33 -0300

    adicionando nuevos cambios

--- app/models.py
+++ app/models.py
@@ -4,18 +4,34 @@ from django.db import models
 class Person(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    phone= models.IntegerField()
-    
+    phone = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 class Evento(models.Model):
-    tipo= models.TextChoices("Casamiento", "Fiesta de 15 años")
-    fecha_evento= models.DateField(auto_now=False, auto_now_add=False)
-    horario=models.TextChoices("Diurno", "Nocturno")
+    TIPO_CHOICES = [
+        ('C', 'Casamiento'),
+        ('O', 'Corporativas'),
+    ]
+    HORARIO_CHOICES = [
+        ('V', 'Vespertino'),
+        ('N', 'Nocturno'),
+    ]
+    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES)
+    fecha_evento = models.DateField(auto_now=False, auto_now_add=False)
+    horario = models.CharField(max_length=1, choices=HORARIO_CHOICES)
+
+    def __str__(self):
+        return f"{self.tipo} - {self.fecha_evento} - {self.horario}" 
+
 
 class Opciones(models.Model):
-    tipo= models.TextChoices("Foto mural", "Video con drone")
+    TIPO_CHOICES = [
+        ('F', 'Foto mural'),
+        ('V', 'Video con drone'),
+    ]
+    tipo = models.CharField(max_length=1, choices=TIPO_CHOICES)
 
-def __str__(self):
-    return self.first_name
-    return self.last_name
-    return self.phone
-    
+    def __str__(self):
+        return self.tipo
